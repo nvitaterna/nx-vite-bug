673921549073b3994dacc1ec074da2c6cc4277db
Nicolas Vitaterna <nvitaterna@gmail.com>
2022-12-22 14:37:47 -0500

    update

--- libs/test-lib/vite.config.ts
+++ libs/test-lib/vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      external: ['react', 'react-dom', 'react/jsx-runtime', 'lodash'],
     },
     minify: false,
   },
